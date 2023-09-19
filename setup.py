de91f6f7786382dfb45eb53ee49b57aeb72c2559
Jason Lau <lau@rapidstream-da.com>
2023-09-19 13:27:42 -0700

    Publish as rapidstream-cocotb-bus on pypi.

--- setup.py
+++ setup.py
@@ -19,18 +19,18 @@ version_file = root / "src" / "cocotb_bus" / "_version.py"
 
 if __name__ == "__main__":
     setup(
-        name="cocotb-bus",
+        name="rapidstream-cocotb-bus",
         version=get_version(version_file),
         author="cocotb maintainers",
         author_email="cocotb@lists.librecores.org",
         description="",
         long_description=readme_file.read_text(encoding="utf-8"),
         long_description_content_type="text/markdown",
-        url="https://github.com/cocotb/cocotb-bus",
+        url="https://github.com/rapidstream-da/cocotb-bus",
         packages=find_packages("src"),
         package_dir={"": "src"},
         install_requires=[
-            "cocotb>=1.5.0.dev,<2.0"
+            "rapidstream-cocotb>=1.5.0.dev,<2.0"
         ],
         python_requires='>=3.5'
     )
